10fbc447d4295fd7566854fd373ce596f1616aec
Nick <ncrider@opentrons.com>
2020-08-25 00:47:34 -0600

    avoid capturing prints in CI

--- ot-versions.py
+++ ot-versions.py
@@ -1,11 +1,13 @@
-from opentrons import __version__ as build
-from opentrons.protocol_api import MAX_SUPPORTED_VERSION as api_level
+import contextlib
+# avoids capturing the container migration prints in the CI/CD stdout
+with contextlib.redirect_stdout(None):
+    from opentrons import __version__ as build
+    from opentrons.protocol_api import MAX_SUPPORTED_VERSION as api_level
 
 version_dir = 'version_info/'
 
 build = str(build)
 api_level = str(api_level)
-
 
 with open( version_dir + 'build.txt', 'w' ) as f:
     f.write(str(build))
